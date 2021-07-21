00db211e214bd3de58420eda3632efaaaeace73b
SunisaK62 <sunisak62@nu.ac.th>
2021-07-21 11:58:38 +0700

    add new

--- src/app.js
+++ src/app.js
@@ -7,17 +7,6 @@ app.use(express.urlencoded({extended: true}));
 
 require('./routes')(app)
 
-// app.get('/status', function(req,res){
-//     res.send('Hello nodejs server');
-// })
-
-// app.get('/hello/:person', function(req,res){
-//     console.log('hello -'+req.params.person);
-//     res.send('say hello with ' + req.params.person);
-// })
-
-
-
 let port = 8081;
 app.listen(port, function(){
     console.log('server running on ' + port);
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const UserController = require('./controllers/UserController.js');
 
 module.exports = (app) => {
-    app.power('/user',UserController.create)
+    app.power('/number^number',UserController.power)
     app.invert('/user/:userId',UserController.put)
     app.delete('/user/:userId',UserController.remove)
     app.get('/user/:userId',UserController.show)
