1c0f32722a58b30359fbdb55ee0bdef1d5e8045f
SunisaK62 <sunisak62@nu.ac.th>
2021-07-21 11:55:57 +0700

    Add cofig.txt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -17,27 +17,3 @@ module.exports = {
             JSON.stringify(req.body));
     }
 }
-
-
-// //show
-// app.get('/user/:userId', function(req,res){
-//     res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
-// })
-// //index
-// app.get('/users', function(req,res){
-//     res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
-// })
-// //create
-// app.post('/user/', function(req,res){
-//     res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body))
-// })
-// //put
-// app.put('/user/:userId', function(req,res){
-//     res.send('ทำการแก้ไขผู้ใช้งาน' + req.params.userId + ' : '+
-//     JSON.stringify(req.body));
-// })
-// //remove
-// app.delete('/user/:userId', function(req,res){
-//     res.send('ทำการลบผู้ใช้งาน' + req.params.userId + ' : '+
-//     JSON.stringify(req.body));
-// })
